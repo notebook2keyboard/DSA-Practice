e6b3f4622ba90268b1177e34cfe50778777aa5e2
Muttu B <muttu2124@Muttus-MacBook-Air.local>
2026-08-22 13:28:18 +0530

    sudoku solver using recursion and backtracking

---
+++ sudokusolver.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool issafe(vector<vector<char>> &board,int row,int col,int dig){
+    for(int j=0;j<9;j++){
+        if(board[row][j]==dig){
+            return false;
+        }
+    }
+    for(int i=0;i<row;i++){
+        if(board[i][col]==dig){
+            return false;
+        }
+    }
+    int strow=(row/3)*3;
+    int stcol = (col/3)*3;
+    for(int i=strow;i<=strow+2;i++){
+        for(int j=stcol;j<=stcol+2;j++){
+            if(board[i][j]==dig){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool sudokusolver(vector<vector<char>> &board,int row,int col){
+    if(row == 9){
+        return true;
+    }
+    int nextrow = row;
+    int nextcol = col+1;
+    if(nextcol == 9){
+        nextrow = row+1;
+        nextcol = 0;
+    }
+    if(board[row][col]!='.'){
+        return sudokusolver(board,nextrow,nextcol);
+    }
+
+    for(char dig='1';dig<='9';dig++){
+        if(issafe(board,row,col,dig)){
+            board[row][col]= dig;
+            if(sudokusolver(board,nextrow,nextcol)){
+                return true;
+            }
+            board[row][col]='.';
+        }
+
+    }
+    return false;
+}
+
+
+    int main() {
+
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+
+    sudokusolver(board, 0, 0);
+
+
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
